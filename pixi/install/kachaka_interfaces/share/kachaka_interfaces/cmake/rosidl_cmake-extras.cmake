791a9713ac1902d0e83d8fb5d2e80353bd038fba
Hiroyuki Okada <okdhryk@gmail.com>
2025-12-03 14:28:50 +0900

    #

--- pixi/install/kachaka_interfaces/share/kachaka_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(kachaka_interfaces_IDL_FILES "action/ExecKachakaCommand.idl;msg/KachakaCommand.idl;msg/Location.idl;msg/LocationList.idl;msg/ObjectDetection.idl;msg/ObjectDetectionListStamped.idl;msg/Shelf.idl;msg/ShelfList.idl;msg/ShelfSize.idl")
-set(kachaka_interfaces_INTERFACE_FILES "action/ExecKachakaCommand.action;msg/KachakaCommand.msg;msg/Location.msg;msg/LocationList.msg;msg/ObjectDetection.msg;msg/ObjectDetectionListStamped.msg;msg/Shelf.msg;msg/ShelfList.msg;msg/ShelfSize.msg")
